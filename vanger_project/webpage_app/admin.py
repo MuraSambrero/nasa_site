246f2c7eebe456da496f4f51d9ea12611ef06ae5
Murasambrero <neones@yandex.ru>
2024-05-08 22:18:25 +0800

    update filer

--- vanger_project/webpage_app/admin.py
+++ vanger_project/webpage_app/admin.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
 from django.utils.safestring import mark_safe
 from .models import SliderImage
 from adminsortable2.admin import SortableAdminMixin
 
 # Register your models here.
 
+
 class SliderAdmin(SortableAdminMixin, admin.ModelAdmin):
-    list_display = ('image', 'get_image', 'my_order')
+    list_display = ("image", "get_image", "my_order")
 
     def get_image(self, object):
         if object.image:
             return mark_safe(f"<img src='{object.image.url}' width=50>")
         else:
-            return 'No Image'
-        
-    get_image.short_description = 'Миниатюра'
+            return "No Image"
+
+    get_image.short_description = "Миниатюра"
+
 
 admin.site.register(SliderImage, SliderAdmin)
