99503cdec64a6e62fe14949e48a4b1efe8878668
Murasambrero <neones@yandex.ru>
2024-05-21 02:55:33 +0800

    create lightbox

--- vanger_project/webpage_app/static/webpage_app/js/scripts.js
+++ vanger_project/webpage_app/static/webpage_app/js/scripts.js
@@ -19,19 +19,12 @@ $(document).on('ready', function() {
     focusOnSelect: true,
   });
 
-  $('img[data-enlargable]').addClass('img-enlargable').click(function(){
-    const src = $(this).attr('src');
-    $('<div>').css({
-        background: 'RGBA(0,0,0,.5) url('+src+') no-repeat center',
-        backgroundSize: 'contain',
-        width:'100%', height:'100%',
-        position:'fixed',
-        zIndex:'10000',
-        top:'0', left:'0',
-        cursor: 'zoom-out'
-    }).click(function(){
-        $(this).remove();
-    }).appendTo('body');
+  const lightbox = GLightbox({
+    selector: "[data-gallery='gallery']",
+    touchNavigation: true,
+    loop: true,
+    autoplayVideos: true
   });
+  lightbox.open();
 });
 
